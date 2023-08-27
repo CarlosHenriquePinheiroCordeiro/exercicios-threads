e92823034a2a273bea499c83e4e1e6e0ae8bba93
CarlosHenriquePinheiroCordeiro <carloshenpincord@gmail.com>
2023-08-27 14:04:04 -0300

    Realização do exercício do agendador de tarefas

---
+++ src/agendador_tarefas/Main.java
@@ -0,0 +1,50 @@
+package agendador_tarefas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int comando          = 0;
+		TimeUnit tipoTempo[] = {TimeUnit.MILLISECONDS, TimeUnit.SECONDS, TimeUnit.MINUTES};
+		List<Tarefa> ls      = new ArrayList<Tarefa>();
+		do {
+			System.out.println("Informe a ação desejada: 1 - Agendar tarefas | 2 - Consultar tarefas | 0 - Encerrar programa");
+			comando = sc.nextInt();
+			if (comando == 2) {
+				System.out.println("\n----------------------------------------------------------------------------");
+				for (Tarefa tarefa : ls) {
+					System.out.println(tarefa);
+				}
+				System.out.println("----------------------------------------------------------------------------");
+			}
+			if (comando == 1) {
+				System.out.println("Informe quantas tarefas deseja agendar");
+				int tarefas = sc.nextInt();
+				for (int i = 0; i < tarefas; i++) {
+					Tarefa t = new Tarefa(i+1);
+					System.out.println("\n----------------------------------------------------------------------------");
+					System.out.println("Tarefa "+(i+1));
+					System.out.println("----------------------------------------------------------------------------");
+					System.out.println("Informe a unidade de tempo: 1 - Milisegundos | 2 - Segundos | 3 - Minutos");
+					t.setTipoTempo(tipoTempo[sc.nextInt()-1]);
+					System.out.println("Informe a quantidade:");
+					t.setTempo(sc.nextInt());
+					ls.add(t);
+				}
+				ScheduledExecutorService ses = Executors.newScheduledThreadPool(tarefas);
+				for (Tarefa tarefa : ls) {
+					ses.schedule(tarefa, tarefa.getTempo(), tarefa.getTipoTempo());
+				}
+				System.out.println("Tarefas agendadas com sucesso!");
+			}
+		} while (comando != 0);
+	}
+
+}
---
+++ src/agendador_tarefas/Tarefa.java
@@ -0,0 +1,72 @@
+package agendador_tarefas;
+
+import java.util.concurrent.TimeUnit;
+
+public class Tarefa implements Runnable {
+
+	private String nome;
+	private int tempo;
+	private TimeUnit tipoTempo = null;
+	private boolean executada = false;
+	
+	public Tarefa(int n) {
+		this.nome = "Tarefa "+n;
+	}
+	
+	@Override
+	public void run() {
+		executada = true;
+		System.out.println(nome+" executada.");
+	}
+	
+	@Override
+	public String toString() {
+		String unidade = "";
+		switch (tipoTempo) {
+			case MILLISECONDS: {
+				unidade = "Milisegundos";
+			}
+			case SECONDS: {
+				unidade = "Segundos";
+			}
+			case MINUTES: {
+				unidade = "Minutos";
+			}
+		}
+		return nome+": Executada - "+isExecutada()+", Unidade - "+unidade+", Quantidade - "+tempo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getTempo() {
+		return tempo;
+	}
+
+	public void setTempo(int tempo) {
+		this.tempo = tempo;
+	}
+
+	public TimeUnit getTipoTempo() {
+		return tipoTempo;
+	}
+
+	public void setTipoTempo(TimeUnit tipoTempo) {
+		this.tipoTempo = tipoTempo;
+	}
+
+	public boolean isExecutada() {
+		return executada;
+	}
+
+	public void setExecutada(boolean executada) {
+		this.executada = executada;
+	}
+	
+
+}
